74f7c54b1f3a5b5af67a081a14fab4acf28ea388
Sean Brennan <spb4578@gmail.com>
2018-11-03 17:11:16 -0500

    added prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,10 +31,38 @@ connection.connect(function(err) {
             console.log(`----------------------------------------------------------------------------------------------------------------------`);
           } else {
             console.log(chalk.red(`Item ${res[i].item_id}`) + `| `+ chalk.blue(`Product Name: ${res[i].product_name}`) + ` --- `+ chalk.green(`Department ${res[i].department_name}`) + ` ---` + chalk.yellow(` Price: ${res[i].price}`) + ` --- Quantity: ${res[i].stock_quantity}`);
-            console.log(`----------------------------------------------------------------------------------------------------------------------git `);
+            console.log(`----------------------------------------------------------------------------------------------------------------------`);
           }
           
       }
        connection.end();
+       start();
     });
+  }
+
+  // function which prompts the user for what action they should take
+function start() {
+    inquirer
+    .prompt([
+        {
+          name: "item",
+          type: "list",
+          message: "What is the item you would like to submit?",
+          choices:["Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7","Item 8","Item 9","Item 10",]
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "how many would you like to buy?",
+         validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ])
+      .then(function(answer) {
+        console.log(chalk.red(`works for ${answer.item} and ${answer.quantity}`));
+      });
   }
